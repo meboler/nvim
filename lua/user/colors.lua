4fa19b758060e4276ee031d00952a0f7d4576d0c
Matt Boler <boler.matthew@gmail.com>
2024-01-12 12:59:18 -0700

    Update color scheme

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -3,7 +3,8 @@
 -------------------------------------
 
 vim.opt.background = 'dark'
-vim.cmd('colorscheme nord')
+vim.cmd('colorscheme catppuccin-macchiato')
+--vim.cmd('colorscheme nord')
 --vim.cmd('colorscheme gruvbox')
 --vim.cmd('colorscheme oxocarbon')
 --vim.cmd('colorscheme base16-atelier-forest')
