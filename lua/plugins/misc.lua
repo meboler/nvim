e11f40f4a1ab0386347d18187629ed5e602cfa05
Matthew Boler <boler.matthew@gmail.com>
2025-07-04 09:18:17 -0600

    Add nvim-surround

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -7,7 +7,10 @@ return {
         ---@type ibl.config
         opts = {},
     },
-    "numToStr/Comment.nvim",
+    {
+        "numToStr/Comment.nvim",
+        opts = {},
+    },
     {
         "kylechui/nvim-surround",
         version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
